59eba3c0684199727a6973907358a084c8f58e32
Abdallah Eliwa <abdalla.eliwa2@hotmail.com>
2019-03-18 21:07:28 +0200

    Initial DB.

---
+++ EcommerceWebsite/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace EcommerceWebsite.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<EcommerceWebsite.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(EcommerceWebsite.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
